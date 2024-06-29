a57f47b2c6ff42ed940348a7f441ae55f5be4583
ChharngChhit <chhit085@gmail.com>
2024-06-29 16:34:38 +0700

    seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -52,170 +52,159 @@ class ProductSeeder extends Seeder
             ]
         );
 
-        $image = [
-            'https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/212303/pexels-photo-212303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/812868/pexels-photo-812868.jpeg?auto=compress&cs=tinysrgb&w=600'
+        $products = [
+            [
+                "type_id"       => 1,
+                "name"          => 'Coca Cola 350ml',
+                "code"          => 'B0009',
+                "image"         => 'pos/product/Coca Cola.png',
+                "unit_price"    => 1.5,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'Dutch Mill Pasturized Low Fat Milk',
+                "code"          => 'D0001',
+                "image"         => 'pos/product/Fat Milk.png',
+                "unit_price"    => 3.5,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'E&V Unsalted Butter 200G',
+                "code"          => 'D0002',
+                "image"         => 'pos/product/Unsalted Butter.png',
+                "unit_price"    => 2.25,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'Angkor beer 330ml',
+                "code"          => 'A0010',
+                "image"         => 'pos/product/Angkor beer.png',
+                "unit_price"    => 1.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 5,
+                "name"          => 'Clear Men Shampoo Scalp Legend by ',
+                "code"          => 'C0002',
+                "image"         => 'pos/product/Clear Men.png',
+                "unit_price"    => 1.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 3,
+                "name"          => 'Giraffe Spiral Notebook A5',
+                "code"          => 'S0002',
+                "image"         => 'pos/product/Notebook.png',
+                "unit_price"    => 0.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'Carne Meats Raw Ribeye Steak',
+                "code"          => 'S0003',
+                "image"         => 'pos/product/Raw Meats.png',
+                "unit_price"    => 0.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'SAMYANG Samyang Hot ',
+                "code"          => 'S0004',
+                "image"         => 'pos/product/SamYang.png',
+                "unit_price"    => 0.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 1,
+                "name"          => 'Coca Cola 350ml',
+                "code"          => 'B0004',
+                "image"         => 'pos/product/Coca Cola.png',
+                "unit_price"    => 1.5,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'Dutch Mill Pasturized Low Fat Milk',
+                "code"          => 'D0004',
+                "image"         => 'pos/product/Fat Milk.png',
+                "unit_price"    => 3.5,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'E&V Unsalted Butter 200G',
+                "code"          => 'D0005',
+                "image"         => 'pos/product/Unsalted Butter.png',
+                "unit_price"    => 2.25,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'Angkor beer 330ml',
+                "code"          => 'A0006',
+                "image"         => 'pos/product/Angkor beer.png',
+                "unit_price"    => 1.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 5,
+                "name"          => 'Clear Men Shampoo Scalp Legend by ',
+                "code"          => 'C0007',
+                "image"         => 'pos/product/Clear Men.png',
+                "unit_price"    => 1.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 3,
+                "name"          => 'Giraffe Spiral Notebook A5',
+                "code"          => 'S0008',
+                "image"         => 'pos/product/Notebook.png',
+                "unit_price"    => 0.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'Carne Meats Raw Ribeye Steak',
+                "code"          => 'S0009',
+                "image"         => 'pos/product/Raw Meats.png',
+                "unit_price"    => 0.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
+            [
+                "type_id"       => 2,
+                "name"          => 'SAMYANG Samyang Hot ',
+                "code"          => 'S0010',
+                "image"         => 'pos/product/SamYang.png',
+                "unit_price"    => 0.95,
+                "created_at"    => Carbon::now(),
+                "updated_at"    => Carbon::now()
+            ],
         ];
 
         // Seed Product
-        // for ($i = 0; $i <100; $i++) {
-        //     DB::table('product')->insert([
-        //         "type_id"       => rand(1,4),
-        //         "name"          => Str::random(10),
-        //         "code"          => Str::random(5),
-        //         "image"         => $image[rand(0,2)],
-        //         "unit_price"    => rand(1, 20)*1000,
-        //         "created_at"    => Carbon::now(),
-        //         "updated_at"    => Carbon::now()
-        // ]);
+        // for ($i = 0; $i < 100; $i++) {
+        //     DB::table('product')->insert($products);
         // }
-
-        DB::table('product')->insert(
-            [
-                "type_id"       => 1,
-                "name"          => 'Coca Cola 350ml',
-                "code"          => 'B0001',
-                "image"         => 'pos/product/Coca Cola.png',
-                "unit_price"    => 1.5,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'Dutch Mill Pasturized Low Fat Milk',
-                "code"          => 'D0001',
-                "image"         => 'pos/product/Fat Milk.png',
-                "unit_price"    => 3.5,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'E&V Unsalted Butter 200G',
-                "code"          => 'D0002',
-                "image"         => 'pos/product/Unsalted Butter.png',
-                "unit_price"    => 2.25,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'Angkor beer 330ml',
-                "code"          => 'A0001',
-                "image"         => 'pos/product/Angkor beer.png',
-                "unit_price"    => 1.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 5,
-                "name"          => 'Clear Men Shampoo Scalp Legend by ',
-                "code"          => 'C0001',
-                "image"         => 'pos/product/Clear Men.png',
-                "unit_price"    => 1.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 3,
-                "name"          => 'Giraffe Spiral Notebook A5',
-                "code"          => 'S0001',
-                "image"         => 'pos/product/Notebook.png',
-                "unit_price"    => 0.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'Carne Meats Raw Ribeye Steak',
-                "code"          => 'S0001',
-                "image"         => 'pos/product/Raw Meats.png',
-                "unit_price"    => 0.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'SAMYANG Samyang Hot ',
-                "code"          => 'S0002',
-                "image"         => 'pos/product/SamYang.png',
-                "unit_price"    => 0.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 1,
-                "name"          => 'Coca Cola 350ml',
-                "code"          => 'B0001',
-                "image"         => 'pos/product/Coca Cola.png',
-                "unit_price"    => 1.5,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'Dutch Mill Pasturized Low Fat Milk',
-                "code"          => 'D0001',
-                "image"         => 'pos/product/Fat Milk.png',
-                "unit_price"    => 3.5,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'E&V Unsalted Butter 200G',
-                "code"          => 'D0002',
-                "image"         => 'pos/product/Unsalted Butter.png',
-                "unit_price"    => 2.25,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'Angkor beer 330ml',
-                "code"          => 'A0001',
-                "image"         => 'pos/product/Angkor beer.png',
-                "unit_price"    => 1.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 5,
-                "name"          => 'Clear Men Shampoo Scalp Legend by ',
-                "code"          => 'C0001',
-                "image"         => 'pos/product/Clear Men.png',
-                "unit_price"    => 1.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 3,
-                "name"          => 'Giraffe Spiral Notebook A5',
-                "code"          => 'S0001',
-                "image"         => 'pos/product/Notebook.png',
-                "unit_price"    => 0.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'Carne Meats Raw Ribeye Steak',
-                "code"          => 'S0001',
-                "image"         => 'pos/product/Raw Meats.png',
-                "unit_price"    => 0.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-            [
-                "type_id"       => 2,
-                "name"          => 'SAMYANG Samyang Hot ',
-                "code"          => 'S0002',
-                "image"         => 'pos/product/SamYang.png',
-                "unit_price"    => 0.95,
-                "created_at"    => Carbon::now(),
-                "updated_at"    => Carbon::now()
-            ],
-        );
+        foreach ($products as $product) {
+            DB::table('product')->insert($product);
+        }
     }
 }
